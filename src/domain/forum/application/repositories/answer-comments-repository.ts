35eb5c2f1f665bbc0dcca64a256d49d12702018c
gabriel.turra@outlook.com <gabriel.turra@outlook.com>
2025-01-07 16:56:53 -0300

    feat(use-case): add fetch-answer-comments use-case

--- src/domain/forum/application/repositories/answer-comments-repository.ts
+++ src/domain/forum/application/repositories/answer-comments-repository.ts
@@ -1,7 +1,9 @@
+import type { PaginationParams } from '@/core/repositories/pagination-params';
 import type { AnswerComment } from '@/domain/forum/enterprise/entities/answer-comment';
 
 export interface AnswerCommentsRepository {
   create(answerComment: AnswerComment): Promise<void>;
   delete(answer: AnswerComment): Promise<void>;
   findById(id: string): Promise<AnswerComment | null>;
+  findManyByAnswerId(questionId: string, params: PaginationParams): Promise<AnswerComment[]>;
 }
