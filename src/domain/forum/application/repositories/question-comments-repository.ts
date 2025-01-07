c27cc5cdf3884e54fccce1e1a28e22f856542058
gabriel.turra@outlook.com <gabriel.turra@outlook.com>
2025-01-07 16:54:49 -0300

    feat(use-case): add fetch-question-comments use-case

--- src/domain/forum/application/repositories/question-comments-repository.ts
+++ src/domain/forum/application/repositories/question-comments-repository.ts
@@ -1,7 +1,9 @@
+import type { PaginationParams } from '@/core/repositories/pagination-params';
 import type { QuestionComment } from '@/domain/forum/enterprise/entities/question-comment';
 
 export interface QuestionCommentsRepository {
   create(questionComment: QuestionComment): Promise<void>;
   delete(answer: QuestionComment): Promise<void>;
   findById(id: string): Promise<QuestionComment | null>;
+  findManyByQuestionId(questionId: string, params: PaginationParams): Promise<QuestionComment[]>;
 }
